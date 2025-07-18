a9877bfaa754a6ef792b3eae12c388d6842c949a
NJOYMENT <thisisnjoyment@gmail.com>
2025-07-19 00:23:54 +0100

    Update food-part-2.js

--- pages/food-part-2.js
+++ pages/food-part-2.js
@@ -1,15 +1,28 @@
-
 export default function Foodpart2() {
   return (
-    <div style={ padding: "2rem", fontFamily: "Arial" }>
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
       <h1>Hi there!</h1>
       <p>Thanks for getting this far and listing your name down.</p>
-      <h2>FOOD PART 2</h2><p>Bring: Mac and Cheese, Kebab Sticks, Patties, Chicken Wings...</p>
-<h3>DIETARY REQUIREMENTS:</h3>
-<p>For anyone bringing food, please ensure to list anything with (egg, nuts or fish in it)...</p>
-<h3>DIETARY REQUESTS:</h3>
-<p>If you have requested either no pork, no fish or have an allergy... Closest station is Norbury Station.</p>
 
+      <h2>FOOD PART 2</h2>
+      <p>
+        We would be grateful if you can bring any of the following: Mac and Cheese, Kebab Sticks,
+        Condiments, Beef or Fish Patties and Chicken Wings.
+      </p>
+
+      <h3>DIETARY REQUIREMENTS:</h3>
+      <p>
+        For anyone bringing food, please ensure to list anything with (egg, nuts or fish in it) —
+        we have multiple people attending with allergies.
+      </p>
+
+      <h3>DIETARY REQUESTS:</h3>
+      <p>
+        If you have requested either no pork, no fish or have an allergy, please make sure NJ,
+        Hillary or Renee know as soon as you arrive. (Heads up!) We won’t be able to do much if you arrive too late!
+        Thanks so much and see you soon!<br />
+        The full address will be listed 24 hrs to the event and the closest station is Norbury Station.
+      </p>
     </div>
   );
 }
