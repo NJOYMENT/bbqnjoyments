629e6130960d34d9c7752ca40701b28d81dfb945
NJOYMENT <thisisnjoyment@gmail.com>
2025-07-19 00:20:33 +0100

    Update bbq-contribution.js

--- pages/bbq-contribution.js
+++ pages/bbq-contribution.js
@@ -1,15 +1,41 @@
-
-export default function Bbqcontribution() {
+export default function BBQContribution() {
   return (
-    <div style={ padding: "2rem", fontFamily: "Arial" }>
-      <h1>Hi there!</h1>
-      <p>Thanks for getting this far and listing your name down.</p>
-      <h2>£5 BBQ CONTRIBUTION</h2><p>Thanks for contributing! Send £5 to: <a href='https://bit.ly/NJMT-BBQ-CON'>Link</a></p>
-<h3>DIETARY REQUIREMENTS:</h3>
-<p>For anyone bringing food, please ensure to list anything with (egg, nuts or fish in it)...</p>
-<h3>DIETARY REQUESTS:</h3>
-<p>If you have requested either no pork, no fish or have an allergy... Closest station is Norbury Station.</p>
+    <div style={{ padding: "2rem", fontFamily: "Arial, sans-serif", lineHeight: "1.6" }}>
+      <h1>Hi!</h1>
+      <p>
+        Thanks for getting this far and listing your name down. Please make sure you read all the
+        information below and if you have any questions give us a shout!
+      </p>
 
+      <h2>£5 BBQ Contribution</h2>
+      <p>
+        AYYE BIG BALLER! Thanks for being down to put some money in to the pot for this BBQ.
+        You can use this link to send over £5:{" "}
+        <a href="https://bit.ly/NJMT-BBQ-CON" target="_blank" rel="noopener noreferrer">
+          https://bit.ly/NJMT-BBQ-CON
+        </a>
+        . This will go towards the food and DJs playing throughout the night.
+        <br />
+        Please note you do not have to bring anything after placing the contribution.
+      </p>
+
+      <h3>DIETARY REQUIREMENTS:</h3>
+      <p>
+        For anyone bringing food, please ensure to list anything with (egg, nuts or fish in it) —
+        we have multiple people attending with allergies.
+      </p>
+
+      <h3>DIETARY REQUESTS:</h3>
+      <p>
+        If you have requested either no pork, no fish or have an allergy, please make sure NJ,
+        Hillary or Renee know as soon as you arrive to ensure you get something to eat.
+        (Heads up!) We won’t be able to do much if you arrive too late!
+      </p>
+
+      <p>
+        Thanks so much and see you soon! <br />
+        The full address will be listed 24 hrs to the event and the closest station is <strong>Norbury Station</strong>.
+      </p>
     </div>
   );
 }
